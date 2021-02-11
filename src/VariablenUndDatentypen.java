96c5bb48f51c3d566dcea63a03084a77df72ae8c
Richard Kolm <richard.kolm@gmx.at>
2021-02-11 15:22:16 +0100

    VariablenUndDatentypen

---
+++ src/VariablenUndDatentypen.java
@@ -0,0 +1,15 @@
+public class VariablenUndDatentypen {
+    public static void main(String[] args) {
+        String text = " HelloWorld! ";
+        System.out.println(text);
+        System.out.println("String-Länge = " + text.length());
+        System.out.println(text.toUpperCase());
+        System.out.println(text.toLowerCase());
+        System.out.println(text.replaceFirst("World", "Codersbay"));
+        System.out.println(text.replaceFirst(" ",""));
+        System.out.println(text.repeat(15));
+        for (int zaehler=1; zaehler <= 15; zaehler++) {
+            System.out.println(text);
+        }
+    }
+}
